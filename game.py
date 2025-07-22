dbf3a53751e191e88bd1142d1ab50de24598c870
Paulo André <pauloandre.franquini@gmail.com>
2025-07-22 00:45:04 -0300

    Refatoramento geral do código para tornar mais simples e funcionar como o previsto

--- game.py
+++ game.py
@@ -23,85 +23,18 @@ def mostrar_tabela(tab):
 
 
 def jogada(x_ou_o):
-    posicao = int(input("Qual posição jogar ?"))
-
-    tabela[posicao]= x_ou_o
-
     mostrar_tabela(tabela)
 
+    print(f"Você está jogando como {x_ou_o}")
 
-def victory_or_draw(jogador):
-    controle = 0
+    posicao = int(input("Qual posição jogar ?"))
 
-    while controle < 1:
-        jogada(jogador.opcao_de_jogo)  # fazer um while loop dentro de um while loop para gerar alternância de turnos
-
-        if tabela[0] and tabela[1] and tabela[2] == "X" or tabela[0] and tabela[1] and tabela[2] == "O":
-            controle += 1
-
-        elif tabela[3] and tabela[4] and tabela[5] == "X" or tabela[3] and tabela[4] and tabela[5] == "O":
-            controle += 1
-
-        elif tabela[6] and tabela[7] and tabela[8] == "X" or tabela[6] and tabela[7] and tabela[8] == "O":
-            controle += 1
-
-        elif tabela[0] and tabela[3] and tabela[6] == "X" or tabela[0] and tabela[3] and tabela[6] == "O":
-            controle += 1
-
-        elif tabela[1] and tabela[4] and tabela[7] == "X" or tabela[1] and tabela[4] and tabela[7] == "O":
-            controle += 1
-
-        elif tabela[2] and tabela[5] and tabela[8] == "X" or tabela[2] and tabela[5] and tabela[8] == "O":
-            controle += 1
-
-        elif tabela[0] and tabela[4] and tabela[8] == "X" or tabela[0] and tabela[4] and tabela[8] == "O":
-            controle += 1
-
-        elif tabela[2] and tabela[4] and tabela[6] == "X" or tabela[2] and tabela[4] and tabela[6] == "O":
-            controle += 1
-
-        else:
-            controle = 0  # adicionar forma do jogo finalizar empatado neste else
-
-def criacao_dos_players():
-
-    x_ou_o = input("Qual você quer ser ?[X ou O]").upper()
-    x_ou_o1 = " "
-
-    if x_ou_o == "X" or x_ou_o == "O":
-        jogador1 = Player(x_ou_o)
-        if x_ou_o == "X":
-            x_ou_o1 == "O"
-            jogador2 = Player(x_ou_o1)
-        else:
-            x_ou_o1 == "X"
-            jogador2 = Player(x_ou_o1)
+    if tabela[posicao] == " " :
+        tabela[posicao]= x_ou_o
     else:
-        print("""__________________________________________
-           \nEscolha apenas entre X ou O !!!\n__________________________________________""")
+        print("\nAcho que você jogou errado\n")
+        jogada(x_ou_o)
 
-        ligacao()
-
-    victory_or_draw(jogador1)
-
-
-
-
-
-def jogada(x_ou_o):
-    posicao = int(input("Qual posição jogar ?"))
-
-    tabela[posicao]= x_ou_o
-
-    mostrar_tabela(tabela)
-
-
-
-def ligacao():
-
-    mostrar_tabela(tabela)
-
-    criacao_dos_players()
 
 
 
@@ -111,9 +44,69 @@ def main():
     numero = input("Se quer rodar o código aperte 0, se não, aperte 1 ")
 
     while int(numero) < 1:
-        ligacao()
+        opcao_de_jogo = input("\nQual você quer ser ?[X ou O]").upper()
 
-        numero = int(input("Quer continuar ?[0 sim/1 não]"))
+        jogador1 = Player(opcao_de_jogo)
+
+        if jogador1.opcao_de_jogo == "X":
+            jogador2 = Player("O")
+
+        else:
+            jogador2 = Player("X")
+
+        players = [jogador1, jogador2]
+
+
+        current_player_index = 0
+
+        controle = 0
+
+        controle_jogada = 0
+
+        while controle < 1:
+            current_player = players[current_player_index]
+
+            jogada(current_player.opcao_de_jogo)
+
+            controle_jogada += 1
+
+            if tabela[0] == "X" and tabela[1] == "X" and tabela[2] == "X" or tabela[0]== "O" and tabela[1]== "O" and tabela[2] == "O":
+                controle += 1
+
+            elif tabela[3] == "X"  and tabela[4] == "X"  and tabela[5] == "X" or tabela[3] == "O" and tabela[4] == "O" and tabela[5] == "O":
+                controle += 1
+
+            elif tabela[6] == "X"  and tabela[7] == "X"  and tabela[8] == "X" or tabela[6] == "O" and tabela[7] == "O" and tabela[8] == "O":
+                controle += 1
+
+            elif tabela[0] == "X" and tabela[3] == "X" and tabela[6] == "X" or tabela[0] == "O" and tabela[3] == "O" and tabela[6] == "O":
+                controle += 1
+
+            elif tabela[1] == "X" and tabela[4] == "X" and tabela[7] == "X" or tabela[1] == "O" and tabela[4] == "O" and tabela[7] == "O":
+                controle += 1
+
+            elif tabela[2] == "X" and tabela[5] == "X" and tabela[8] == "X" or tabela[2] == "O" and tabela[5] == "O" and tabela[8] == "O":
+                controle += 1
+
+            elif tabela[0] == "X" and tabela[4] == "X" and tabela[8] == "X" or tabela[0] == "O" and tabela[4] == "O" and tabela[8] == "O":
+                controle += 1
+
+            elif tabela[2] == "X" and tabela[4] == "X" and tabela[6] == "X" or tabela[2] == "O" and tabela[4] == "O" and tabela[6] == "O":
+                controle += 1
+
+            elif controle_jogada == 9 :
+                controle += 1
+                print("\n")
+                mostrar_tabela(tabela)
+                print("Empate\n")
+
+            else:
+                print("")
+
+            # Advance to the next player
+            current_player_index = (current_player_index + 1) % len(players)
+
+        numero = int(input("\nQuer continuar ?[0 sim/1 não]"))
 
         if numero == 0:
             numero == 0
